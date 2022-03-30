ee0bc0dfb6a001f70c27e1e4b102f489bd7f28dc
senlief <zjanence@outlook.com>
2022-03-31 01:57:45 +0800

    增加了多个知识库的同步

--- app.py
+++ app.py
@@ -5,25 +5,24 @@ from task import publish_doc, delete_doc, update_doc
 
 app = FastAPI()
 
-@app.post("/", status_code=200)
-def yuque(data: dict, background_tasks: BackgroundTasks):
+@app.post("/{prefix}", status_code=200)
+def yuque(data: dict, prefix: str, background_tasks: BackgroundTasks):
     req = data['data']   
     type = req['webhook_subject_type']
     format = req.get('format', '')
     if format == 'lake':
         if type == 'publish':
-            background_tasks.add_task(publish_doc, req['slug'], req['body'])
+            background_tasks.add_task(publish_doc, req['slug'], req['body'], prefix)
         elif type == 'update':
-            background_tasks.add_task(update_doc, req['slug'], req['body'])
+            background_tasks.add_task(update_doc, req['slug'], req['body'], prefix)
         else:
             print("未知的请求TYPE")
         return {"msg": "收到了Webhook的请求！"}
     elif type == 'comment_create' and req['actor_id'] == req['commentable']['user_id']:
-            background_tasks.add_task(delete_doc, req['commentable']['slug'])
+            background_tasks.add_task(delete_doc, req['commentable']['slug'], prefix)
     else:
         print("不支持的格式")
         return {"msg": "不支持解析的格式！"}
-
 
 if __name__ == '__main__':
     uvicorn.run(app=app, host="127.0.0.1", port=8080)
--- task.py
+++ task.py
@@ -15,52 +15,55 @@ from yuque import YuQue
 from lake2md import hugo_lake_to_md
 
 
-try:
-    config = dotenv_values(".env")
-    token = config['TOKEN']
-    desdir = config['DESDIR']
-    namespace = config['NAMESPACE']
-    cmd = config['CMD']
-    workdir = config['WORKDIR']
-    html = config['HTML']
-except Exception as e:
-    print(f"环境变量配置错误：{0}", e)
+class Config:
+    def __init__(self, prefix):
+        try:
+            pwd = Path(__file__).absolute()
+            file_path = Path(pwd).parent / 'config.json'
+            with open(file_path, 'r') as f:
+                config = json.load(f)
 
+            if prefix in config.keys():
+                self.token = config[prefix]['token']
+                self.namespace = config[prefix]['namespace']
+                self.desdir = config[prefix]['desdir']
+                self.workdir = config[prefix]['workdir']
+                self.cmd = config[prefix]['cmd']
+                self.html = config[prefix]['html']
+            else:
+                print("配置不正确！")
+        except OSError as e:
+            print(e)
 
-def deploy(func):
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        func1 = func(*args, **kwargs)
-        dir = Path(workdir)
-        if not Path(dir).exists():
-            print("工作目录不存在")
+    def deploy(self):
+        if self.cmd != '':
+            os.chdir(self.workdir)
+            return subprocess.Popen(self.cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,encoding="utf-8")
         else:
-            os.chdir(dir)
-        subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,encoding="utf-8")
-        return func1
-    return wrapper
+            print('没有命令可以执行')
+        
 
-
-def init_doc():
-    yq = YuQue(token)
-    trees = yq.get_info(namespace)
+def init_doc(prefix):
+    config = Config(prefix)
+    yq = YuQue(config.token)
+    trees = yq.get_info(config.namespace)
 
     # 把目录写到本地，以便于更新和删除
-    tree_path = Path(desdir, 'yuque.json')
+    tree_path = Path(config.desdir, 'yuque.json')
     with open(tree_path, 'w+') as f:
         json.dump(trees, f, indent=6)
     
     # 遍历目录树
     for tree in trees:
-        path = Path(desdir, tree['path'])
+        path = Path(config.desdir, tree['path'])
         if Path(path).exists():
                 pass
         else:
             Path(path).mkdir(parents=True)
-        doc = yq.get_doc(namespace, tree['slug'])
+        doc = yq.get_doc(config.namespace, tree['slug'])
 
         if doc['format'] == 'lake':
-            file = Path(desdir, Path(tree['path']), tree['title'] + '.md')
+            file = Path(config.desdir, Path(tree['path']), tree['title'] + '.md')
             with open(file, 'w') as f:
                 md_doc = hugo_lake_to_md(doc['body'], doc['title'])
                 f.writelines(md_doc)
@@ -69,49 +72,59 @@ def init_doc():
             print(doc['title'] + '---' + "格式不支持跳过")
 
 
-@deploy
-def publish_doc(slug, doc):
+
+def publish_doc(slug, doc, prefix):
+    config = Config(prefix)
     # 获取目录列表
-    yq = YuQue(token)
-    trees = yq.get_info(namespace)
-    tree_path = Path(desdir, 'yuque.json')
+    yq = YuQue(config.token)
+    trees = yq.get_info(config.namespace)
+    tree_path = Path(config.desdir, 'yuque.json')
     with open(tree_path, 'w+') as f:
         json.dump(trees, f, indent=6)
 
     for tree in trees:
-        path = Path(desdir, tree['path'])
-        if Path(path).exists():
-                pass
+        if tree['slug'] == slug:
+            path = Path(config.desdir, tree['path'])
+            if Path(path).exists():
+                    pass
+            else:
+                Path(path).mkdir(parents=True)
+            file = Path(config.desdir, Path(tree['path']), tree['title'] + '.md')
+            with open(file, 'w') as f:
+                md_doc = hugo_lake_to_md(doc, tree['title'], html=config.html)
+                f.writelines(md_doc)
+            title = tree['title']
         else:
-            Path(path).mkdir(parents=True)
-        doc = yq.get_doc(namespace, tree['slug'])
-        file = Path(desdir, Path(tree['path']), tree['title'] + '.md')
-        with open(file, 'w') as f:
-            md_doc = hugo_lake_to_md(doc['body'], tree['title'])
-            f.writelines(md_doc)
+            pass
+    
+    config.deploy()
+    print(f"知识库{config.namespace}发布了一遍名为<<{title}>>的文章并已部署！")
 
 
-@deploy
-def delete_doc(slug):
-    tree_path = Path(desdir, 'yuque.json')
+def delete_doc(slug, prefix):
+    config = Config(prefix)
+    tree_path = Path(config.desdir, 'yuque.json')
     with open(tree_path, 'r') as f:
         trees = json.load(f)
     
     for tree in trees:
         if tree['slug'] == slug:
-            path = Path(desdir, tree['path'], tree['title'] + '.md')
+            path = Path(config.desdir, tree['path'], tree['title'] + '.md')
             Path(path).unlink()
+            title = tree['title']
+    config.deploy()
+    print(f"知识库{config.namespace}删除了一篇名为<<{title}>>的文章!")
 
 
-@deploy
-def update_doc(slug, doc):
-    tree_path = Path(desdir, 'yuque.json')
+def update_doc(slug, doc, prefix):
+    config = Config(prefix)
+    tree_path = Path(config.desdir, 'yuque.json')
     with open(tree_path, 'r') as f:
         trees = json.load(f)
     
     for tree in trees:
         if tree['slug'] == slug:
-            path = Path(desdir, tree['path'])
+            path = Path(config.desdir, tree['path'])
             if Path(path).exists():
                 pass
             else:
@@ -119,14 +132,19 @@ def update_doc(slug, doc):
                 print("文档已被修改或移动，直接覆盖")
             file = Path(path, tree['title'] + '.md')
             with open(file, 'w') as f:
-                md_doc = hugo_lake_to_md(doc, tree['title'], html=html)
+                md_doc = hugo_lake_to_md(doc, tree['title'], html=config.html)
                 f.writelines(md_doc)
+            title = tree['title']
         else:
             pass
+    config.deploy()
+    print(f"知识库{config.namespace}更新了一篇<<{title}>>的文章！")
 
    
 if __name__ == '__main__':
-    # publish_doc('qt8h7t', '<a name=\"x9d6C\"></a>\n## 这篇文档只是用来测试的e\n\n', '这是一篇测试文档')
-    # update_doc('qt8h7t', '<a name=\"x9d6C\"></a>\n## 更新了一遍文章\n\n', '这是一篇测试文档')
-    # delete_doc('qt8h7t', '这是一篇测试文档')
-    init_doc()    
+    # publish_doc('ubuab7', '<a name=\"x9d6C\"></a>\n## 这篇文档只是用来测试的e\n\n', 'uuid2')
+    # time.sleep(3)
+    # update_doc('ubuab7', '<a name=\"x9d6C\"></a>\n## 更新了一遍文章\n\n', 'uuid2')
+    # time.sleep(3)
+    # delete_doc('ubuab7', 'uuid2')
+    init_doc('uuid2')    
